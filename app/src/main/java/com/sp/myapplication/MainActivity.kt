1980ce2f397bc06cb0d80760b60cb25a1ace6f22
Daniel Wilson <daniel@loylap.com>
2021-04-30 17:45:32 +0100

    Tiny log bug - click the FAB button 3 times and some logs are missing.

---
+++ app/src/main/java/com/sp/myapplication/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.sp.myapplication
+
+import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import org.tinylog.configuration.Configuration
+import org.tinylog.kotlin.Logger
+
+class MainActivity : AppCompatActivity() {
+
+    private var clicks = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Configuration.set("writer.file", filesDir.path + "/logs/tiny_log_{count}.txt")
+
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.toolbar))
+
+        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
+            clicks++
+            for (i in 0..15000) {
+                Logger.debug("Log entry number ($clicks) $i")
+            }
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
